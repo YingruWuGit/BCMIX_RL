03a4f89de8f93035b86b76f5b964b409bd88862c
Yingru <arantilwu@gmail.com>
2025-09-19 17:37:43 +0100

    initial

--- bcmix.py
+++ bcmix.py
@@ -9,7 +9,7 @@ W = 0
 GAMMA = 0.9
 LIM = 31
 M1 = 4
-M2 = 3
+M2 = 3 # previous highest
 N = 100
 """
 XSTAR = 1.9
@@ -18,7 +18,7 @@ W = 0.1
 GAMMA = 0.9
 LIM = 31
 M1 = 6
-M2 = 6
+M2 = 4 # previous highest
 N = 100
 """
 
@@ -46,7 +46,7 @@ def myopic(canonical, precision, xstar=None):
 
 def gain(canonical, precision, x, xstar=None):
     """
-    conditional expectation of immediate reward without change point
+    expected immediate reward condition on s, x without change point
     """
     xstar = XSTAR if xstar is None else xstar
     covm = np.linalg.inv(precision)
@@ -59,6 +59,7 @@ def gain(canonical, precision, x, xstar=None):
 def myopic_mix(states, p, xstar=None):
     """
     myopic policy with change point
+    states: posterior of previous time
     """
     xstar = XSTAR if xstar is None else xstar
     bamys, bsquare, pit = [], [], []
@@ -79,7 +80,8 @@ def myopic_mix(states, p, xstar=None):
 
 def gain_mix(states, p, x=None, xstar=None):
     """
-    conditional expectation of immediate reward with change point
+    expected immediate reward condition on s, x with change point
+    states: posterior of previous time
     """
     xstar = XSTAR if xstar is None else xstar
     bamys, bsquare, pit = [], [], []
@@ -117,7 +119,7 @@ def env_response(x, alpha, beta, mean_true=None, covm_true=None, p=0, err=None):
 
 def update_without_change(canonical, precision, x, y):
     """
-    update state(canonical precision) after observe x and y, assume no change point
+    update state(canonical precision) after observe x and y, no change point
     params:
         canonical, precision: state at current step
         x, y: observations
@@ -134,27 +136,27 @@ def q_myopic_without_change(canonical, precision, x, xstar=None):
         canonical, precision: state at current step
         x: action
     """
+    totreward = np.zeros(N)
+    canonical_batch = [canonical.copy() for _ in range(N)]
+    precision_batch = [precision.copy() for _ in range(N)]
+    xstar_batch = np.full(N, xstar)
+    x_batch = np.full(N, x)
+    totreward += gain(canonical, precision, x, xstar)
+
     covm = np.linalg.inv(precision)
     mean = covm @ canonical
     thetas = np.random.multivariate_normal(mean.flatten(), covm, size=N)
     alpha_batch = thetas[:, 0]
     beta_batch = thetas[:, 1]
 
-    totreward = np.zeros(N)
-    canonical_batch = [canonical.copy() for _ in range(N)]
-    precision_batch = [precision.copy() for _ in range(N)]
-    xstar_batch = np.full(N, XSTAR) if xstar is None else np.full(N, xstar)
-    x_batch = np.full(N, x)
-    totreward += gain(canonical, precision, x, xstar)
-
     for i in range(1, LIM):
-        # y_batch from t=i-1
-        y_batch = alpha_batch + x_batch * beta_batch + np.random.normal(0.0, 1.0, N)
+        # y_batch from t = i - 1
+        y_batch = alpha_batch + x_batch * beta_batch + np.random.normal(0.0, 1.0, N) # equal to env_response
         for j in range(N):
             canonical_batch[j], precision_batch[j] = update_without_change(
                 canonical_batch[j], precision_batch[j], x_batch[j], y_batch[j]
             )
-            xstar_batch[j] = XSTAR if xstar is None else x_batch[j] # if xstar not None use x_{t-1}
+            xstar_batch[j] = xstar_batch[j] if xstar is None else x_batch[j] # if xstar not None use x_{t-1}
             myp = myopic(canonical_batch[j], precision_batch[j], xstar_batch[j])
             x_batch[j] = myp[0]
             totreward[j] += (GAMMA ** i) * myp[1]
@@ -162,9 +164,9 @@ def q_myopic_without_change(canonical, precision, x, xstar=None):
 
 def update_with_change(states, x, y, p):
     """
-    update states(canonical precision logcon pit) after observe x and y, assume changes
-    parameters:
-        states: states at current step, a set of fixed num of mixtures, M1 latest, M2 previous highest
+    update posterior states(canonical precision logcon pit) after observe x and y, assume change points
+    params:
+        states: posterior from the previous time
         x, y: observations
         p: probability of change point
     """
@@ -180,11 +182,12 @@ def update_with_change(states, x, y, p):
     states_t[-1]["can"], states_t[-1]["pre"] = update_without_change(states[0]["can"], states[0]["pre"], x, y)
     states_t[-1]["log"] = (np.linalg.slogdet(states_t[-1]["pre"])[1] - (states_t[-1]["can"].T @ np.linalg.inv(states_t[-1]["pre"]) @ states_t[-1]["can"]).item()) / 2
     logpit_s[-1] = np.log(p) + (states[0]["log"] - states_t[-1]["log"])
+
     max_logpit_t = np.max(logpit_s)
     pit_t = np.exp(np.array(logpit_s) - max_logpit_t)
-
     for i in states_t.keys():
         states_t[i]["pit"] = pit_t[i]
+
     if len(states_t) <= M1 + M2 + 1:
         # num mix <= bound
         states_t[len(states_t)] = states_t.pop(-1)
@@ -203,28 +206,39 @@ def update_with_change(states, x, y, p):
         ans[i]["pit"] /= pit_sum
     return ans
 
-def q_myopic_with_change(states, x, alpha, beta, mean_true, covm_true, p, xstar=None):
+def q_myopic_with_change(states, x, p, xstar=None):
     """
-    calculate Q function of states and action following myopic policy
-    parameters:
-        states: states at current step
+    calculate Q function of state and action following myopic policy, assume change point
+    params:
+        states: posterior from the previous time
         x: action
-        alpha, beta: previous env parameters, might change at this step
-        mean_true, covm_true: how alpha beta generated
         p: probability of change point
     """
     estimates = np.zeros(N)
     for n in range(N):
         # initialize
-        totreward = 0
-        states_i, x_i, alpha_i, beta_i = copy.deepcopy(states), x, alpha, beta
-        xstar_i = xstar
-        for i in range(LIM):
-            y_i, alpha_i, beta_i = env_response(x_i, alpha_i, beta_i, mean_true, covm_true, p)
-            totreward += (GAMMA ** i) * reward(x_i, y_i, xstar_i)
-            # next state
-            states_i = update_with_change(states_i, x_i, y_i, p)
-            xstar_i = None if xstar is None else x_i
-            x_i = myopic_mix(states_i, p, xstar_i)
-        estimates[n] = totreward
+        states_n = copy.deepcopy(states)
+        x_n, xstar_n = x, xstar
+        totgain = gain_mix(states_n, p, x_n, xstar_n)
+
+        # true alpha beta from the previous time
+        covm_0 = np.linalg.inv(states_n[0]["pre"])
+        mean_0 = covm_0 @ states_n[0]["can"]
+        if len(states_n) == 1:
+            alpha_n, beta_n = np.random.multivariate_normal(mean_0.flatten(), covm_0)
+        else:
+            ic = np.random.choice([_ for _ in states_n.keys() if _ != 0], p=[s["pit"] for _, s in states_n.items() if _ != 0])
+            covm_ic = np.linalg.inv(states_n[ic]["pre"])
+            mean_ic = covm_ic @ states_n[ic]["can"]
+            alpha_n, beta_n = np.random.multivariate_normal(mean_ic.flatten(), covm_ic)
+
+        for i in range(1, LIM):
+            # y_n from t = i - 1
+            y_n, alpha_n, beta_n = env_response(x_n, alpha_n, beta_n, mean_0, covm_0, p)
+            states_n = update_with_change(states_n, x_n, y_n, p)
+            xstar_n = xstar_n if xstar is None else x_n # if xstar not None use x_{t-1}
+            myp = myopic_mix(states_n, p, xstar_n)
+            x_n = myp[0]
+            totgain += (GAMMA ** i) * myp[1]
+        estimates[n] = totgain
     return np.mean(estimates)
